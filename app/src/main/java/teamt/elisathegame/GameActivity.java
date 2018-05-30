cdad054e4098963f91acded04d4b6a00e13f58db
lasseko645 <31951011+lasseko645@users.noreply.github.com>
2018-05-30 12:00:10 +0200

    jeg har lavet lidt om på layout af vores spil, ikke sikker på om det er skide godt men nu er der ihvert styr på at teksten ikke går ud over kanten på checkboxene, og jeg har også added RandomOpgave klassen som skulle have til formål at gøre at vores program kan finde en random opgave fra databasen, jeg har dog kun skrevet logikken og tested at det virker (database er dog ikke knyttet på indu)

--- app/src/main/java/teamt/elisathegame/GameActivity.java
+++ app/src/main/java/teamt/elisathegame/GameActivity.java
@@ -2,12 +2,24 @@ package teamt.elisathegame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+
+        //this secktion is intirely for testing (Lasse from Team T)
+        //right here test to see if the checkbox call works properly
+        RandomOpgave randomOpgave = new RandomOpgave();
+        CheckBox anwserOption_1 = findViewById(R.id.checkBox4);
+        anwserOption_1.setText(randomOpgave.findRandomQuestion("Question_", 15));
+
     }
 }
---
+++ app/src/main/java/teamt/elisathegame/RandomOpgave.java
@@ -0,0 +1,59 @@
+package teamt.elisathegame;
+
+import java.util.Random;
+
+public class RandomOpgave {
+
+
+        //this class has to task to make the program able to find a random assignment from the database
+        //in order to do that we have to first make it able to keep track of certain key variables
+        //then we will have to make a method that the rest of the program cam use to find a random question in our database
+
+        //her er de variabler som er nødvendige for at få det her til at virke
+
+        //this is for use in the random method to find a random question in our intire DB
+        //here we will put the referance code for the DB
+        private int amountOfTotalQuestionsInDatabase = 17;
+
+        //this will be the kontroll variable for the program to make sure there is alway talking about the same
+        private int questionControlVariableHolder = 99999999;
+
+        //we need a variable that can hold all of this in a string so we can use it in firebase
+        private String DBQuestionReferanse = "Question_";
+
+        // now the methods that we are gonna use are one random method to find a random question taking referance from our AmountOfTotalQuestionsInDatabase variable
+        // and we are gonna use slightly more complicated get method for the questionControlVariableHolder value since it is not gonna be stored here
+
+        //this is the random method that will find our random question
+        public String findRandomQuestion( String DBQuestionReferanseHolder, int amountOfTotalQuestionsInDatabaseHolder) {
+
+            //for good messure ill load our value into a lokal holder first for both values
+            int TotalQuestionsHolder = amountOfTotalQuestionsInDatabaseHolder;
+            int controlVarHolder = 9999999;
+            amountOfTotalQuestionsInDatabase = amountOfTotalQuestionsInDatabaseHolder;
+            DBQuestionReferanse = DBQuestionReferanseHolder;
+
+
+            //here i use the previous value to find a random question to put into our controlvariable
+            Random randomQuestion = new Random();
+            controlVarHolder =  randomQuestion.nextInt(TotalQuestionsHolder+1);
+            questionControlVariableHolder = controlVarHolder;
+            Integer.toString(controlVarHolder);
+            DBQuestionReferanse = DBQuestionReferanse + controlVarHolder;
+
+            return DBQuestionReferanse;
+        }
+
+        public int getAmountOfTotalQuestionsInDatabase() {
+            return amountOfTotalQuestionsInDatabase;
+        }
+
+        public int getQuestionControlVariableHolder() {
+            return questionControlVariableHolder;
+        }
+
+        public String getDBQuestionReferanse() {
+            return DBQuestionReferanse;
+        }
+    }
+
