3ee910db8249eefcf350163f46378b95d723141f
kasp4770 <kasp4770@edu.easj.dk>
2018-05-30 14:41:04 +0200

    RandomRightAnswer klassen oprettet

---
+++ app/src/main/java/teamt/elisathegame/bussinesslogic/RandomRightAnswer.java
@@ -0,0 +1,28 @@
+package teamt.elisathegame.bussinesslogic;
+
+/*
+* This class has the task to get one random right answer from the database
+* */
+
+public class RandomRightAnswer {
+
+    private int questionControlVarTemp;
+
+    private int DBQuestionReferenceTemp;
+
+    private int amountOfTotalRightAnswerTemp;
+
+
+    /* DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference questions = dbRef.child("Questions");
+    DatabaseReference question_ = questions.child("Question_");
+    DatabaseReference questDesc = question_.child("questDesc");*/
+
+
+/*
+    public String findOneRandomRightAnswer(){
+        Database
+
+        return;
+    }*/
+}
