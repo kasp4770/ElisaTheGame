2a00e101bf382b688a9b6a63a2f625e5a0d9c87c
LiselotteEL <lise1066@edu.easj.dk>
2018-05-31 16:14:43 +0200

    Added RandomWrongAnswer class and needed variables

---
+++ app/src/main/java/teamt/elisathegame/bussinesslogic/RandomWrongAnswer.java
@@ -0,0 +1,13 @@
+package teamt.elisathegame.bussinesslogic;
+
+/*
+This purpose of this class is to randomly select a wrong answer from our database and to repeat this process
+until the needed 5 wrong answers have been selected
+*/
+public class RandomWrongAnswer {
+    private int questionControlVarTemp;
+    private String DBQuestionReferenceTemp;
+    private String[] wrongAnswetLogTemp;
+    private int amountOfTotalWrongAnswers;
+
+}
