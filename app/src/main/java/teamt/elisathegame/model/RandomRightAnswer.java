ceed72719dedaaae9567ca22e284a402e6fe97c6
kasp4770 <kasp4770@edu.easj.dk>
2018-06-01 15:09:46 +0200

    RandomRightAnswer færdig, mangler dog kommentarer/noter

--- app/src/main/java/teamt/elisathegame/model/RandomRightAnswer.java
+++ app/src/main/java/teamt/elisathegame/model/RandomRightAnswer.java
@@ -4,25 +4,23 @@ package teamt.elisathegame.model;
 * This class has the task to get one random right answer from the database
 * */
 
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.Random;
+
 public class RandomRightAnswer {
-
-    private int questionControlVarTemp;
-
-    private int DBQuestionReferenceTemp;
 
     private int amountOfTotalRightAnswerTemp;
 
+    public String findOneRandomRightAnswer(DB_Connect con, String DBQuestionReference, String amountOfRightAnswers){
+        String rightAnswer = "rightAnswer_";
+        DatabaseReference DBAmount = con.findAmountOfRightAnswers(DBQuestionReference, amountOfRightAnswers);
+        String DBAmountRightAnswerString = DBAmount.toString();
+        amountOfTotalRightAnswerTemp = Integer.parseInt(DBAmountRightAnswerString);
 
-    /* DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference questions = dbRef.child("Questions");
-    DatabaseReference question_ = questions.child("Question_");
-    DatabaseReference questDesc = question_.child("questDesc");*/
+        Random random = new Random();
+        int randomRightAnswerNo = random.nextInt(amountOfTotalRightAnswerTemp);
 
-
-/*
-    public String findOneRandomRightAnswer(){
-        Database
-
-        return;
-    }*/
+        return rightAnswer + Integer.toString(randomRightAnswerNo);
+    }
 }
